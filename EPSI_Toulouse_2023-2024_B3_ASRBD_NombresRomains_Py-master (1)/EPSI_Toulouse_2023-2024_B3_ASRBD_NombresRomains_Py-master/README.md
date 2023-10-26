466728283412e5dae6b9b317b24b42945947afc5
YanisLmbrgt <118165715+YanisLmbrgt@users.noreply.github.com>
2023-10-26 15:34:53 +0200

    Delete EPSI_Toulouse_2023-2024_B3_ASRBD_NombresRomains_Py-master (1)/EPSI_Toulouse_2023-2024_B3_ASRBD_NombresRomains_Py-master directory

--- EPSI_Toulouse_2023-2024_B3_ASRBD_NombresRomains_Py-master (1)/EPSI_Toulouse_2023-2024_B3_ASRBD_NombresRomains_Py-master/README.md
+++
@@ -1,1 +0,0 @@
-# NombresRomains_Py
--- EPSI_Toulouse_2023-2024_B3_ASRBD_NombresRomains_Py-master (1)/EPSI_Toulouse_2023-2024_B3_ASRBD_NombresRomains_Py-master/src/ConvertisseurNombresRomains.py
+++
@@ -1,6 +0,0 @@
-class ConvertisseurNombresRomains:
-    @classmethod
-    def convertir(cls, nombre_arabe):
-        if(nombre_arabe == 1): return "I"
-        if(nombre_arabe == 2): return "II"
-        return "III"
